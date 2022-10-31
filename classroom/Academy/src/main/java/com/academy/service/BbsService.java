2e9153a1803128e7aa19dd91b6b4ae3112305aae
admin <admin@DESKTOP-A3NNEC1>
2022-10-31 16:40:52 +0900

    10월31 서비스 테스트

--- classroom/Academy/src/main/java/com/academy/service/BbsService.java
+++ classroom/Academy/src/main/java/com/academy/service/BbsService.java
@@ -1,5 +1,20 @@
 package com.academy.service;
 
+import java.util.List;
+
+import com.academy.domain.BbsVO;
+
+
 public interface BbsService {
 
+	public void register(BbsVO board);
+
+	public BbsVO get(Long bno);
+
+	public boolean modify(BbsVO board);
+
+	public boolean remove(Long bno);
+
+	public List<BbsVO> getList();
+	
 }
